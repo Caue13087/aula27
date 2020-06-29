889ac708fcffe83eebf0e074e8eb8d8b322edd57
Caue13087 <caue13087@gmail.com>
2020-06-29 16:16:39 -0300

    aula27

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Aula27_28_29_30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Produto p1 = new Produto();
+            p1.Codigo = 1;
+            p1.Nome = "Gibson";
+            p1.Preco = 4500f;
+
+            p1.Cadastrar(p1);
+        }
+    }
+}
